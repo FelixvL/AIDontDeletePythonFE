1b8ba9fb4963452cdf557f8d6b9b4b04c94d00de
Lon Lrn <lonlrn@Lons-MacBook-Pro.local>
2024-08-13 12:19:58 +0200

    eerste versie lvs

--- gegevens.js
+++ gegevens.js
@@ -1,2 +1,2 @@
-var url = "https://aidontdeletepython.azurewebsites.net"
-//var url = "http://127.0.0.1:5000"
+//var url = "https://aidontdeletepython.azurewebsites.net"
+var url = "http://127.0.0.1:5000"
